ab3d2ac76ca0d656a964caf5a316cab8599f5056
Velial72 <126498671+Velial72@users.noreply.github.com>
2023-03-24 21:43:49 +0500

    Update bot.py
    добавил цикл while

--- bot.py
+++ bot.py
@@ -4,14 +4,16 @@ import os
 import random
 import time
 
-load_dotenv()
-token = os.getenv('TG_TOKEN')
-rate = os.getenv('RATE_OF_PUBLICATION')
-folder_path = os.path.join('images')
-bot = telegram.Bot(token=token)
-tree = os.walk('images')
+while True:
+    load_dotenv()
+    token = os.getenv('TG_TOKEN')
+    rate = os.getenv('RATE_OF_PUBLICATION')
+    folder_path = os.path.join('images')
+    bot = telegram.Bot(token=token)
+    tree = os.walk('images')
 
-for root, catalog, images in tree:
-    for image in images:
-        bot.send_document(chat_id='@space464', document=open(f'{folder_path}/{image}', 'rb'), timeout=60)
-        time.sleep(int(rate))
+    for root, catalog, images in tree:
+        for image in images:
+            bot.send_document(chat_id='@space464', document=open(f'{folder_path}/{image}', 'rb'), timeout=60)
+            time.sleep(int(rate))
+        random.shuffle(images)
