1ec81f4f099ff543d5f2f67b57c6c6bbf166d2b3
Velial72 <126498671+Velial72@users.noreply.github.com>
2023-03-27 20:10:20 +0300

    Update main.py
    исправил запуск скриптов

--- main.py
+++ main.py
@@ -1,30 +1,28 @@
-from save_image import save_image, checking_the_extension
 from dotenv import load_dotenv
-import os
 import argparse
-import sys
-from fetch_nasa_epic import fetch_nasa_epic
-from fetch_nasa_apod import fetch_nasa_apod
-from fetch_spacex_last_launch import fetch_spacex_last_launch
-
-
-def main():
-    parser = argparse.ArgumentParser(description='Введите откуда скачиваем картинки и/или ID последнего запуска')
-    parser.add_argument('function', nargs='?', help='название функции', default='all')
-    parser.add_argument('id', nargs='?', help='ID запуска', default='latest')
-    entered_values = parser.parse_args()
-
-    if entered_values.function == 'fetch_nasa_epic':
-        fetch_nasa_epic()
-    elif entered_values.function == 'fetch_nasa_apod':
-        fetch_nasa_apod()
-    elif entered_values.function == 'fetch_spacex_last_launch':
-        fetch_spacex_last_launch()
-    else:
-        fetch_spacex_last_launch()
-        fetch_nasa_apod()
-        fetch_nasa_epic()
+import os
+import telegram
+import random
+import time
 
 
 if __name__ == '__main__':
-    main()
+    load_dotenv()
+    parser = argparse.ArgumentParser(description='Введите: ID канала телеграм')
+    parser.add_argument('id_telegram', nargs='?', help='ID канала')
+    entered_values = parser.parse_args()
+    while True:
+        load_dotenv()
+        token = os.getenv('TG_TOKEN')
+        rate = os.getenv('RATE_OF_PUBLICATION')
+        folder_path = os.path.join('images')
+        bot = telegram.Bot(token=token)
+        tree = os.walk('images')
+
+        for root, catalog, images in tree:
+            for image in images:
+                path_to_image = os.path.join(root, image)
+                with open(path_to_image, 'rb') as file:
+                    bot.send_document(chat_id=entered_values.id_telegram, document=(file), timeout=60)
+                time.sleep(int(rate))
+            random.shuffle(images)
