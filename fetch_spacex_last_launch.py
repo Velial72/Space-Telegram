efed5a931eefad2f785fd61e7f200c1bb81cb651
Velial72 <126498671+Velial72@users.noreply.github.com>
2023-03-27 20:12:20 +0300

    Update fetch_spacex_last_launch.py
    убрал лишнее из кода

--- fetch_spacex_last_launch.py
+++ fetch_spacex_last_launch.py
@@ -1,27 +1,19 @@
-from save_image import *
-import argparse
-import sys
+import requests
 import os
+from save_image import save_image
 
 
-def fetch_spacex_last_launch():
-    parser = argparse.ArgumentParser(description='Введите ID запуска')
-    parser.add_argument('id', nargs='?', help='ID запуска', default='latest')
-    launch_id = parser.parse_args(sys.argv[1:])
-    api_space = f'https://api.spacexdata.com/v5/launches/{launch_id.id}'
+def fetch_spacex_last_launch(entered_values):
+    api_space = f'https://api.spacexdata.com/v5/launches/{entered_values}'
     filename = 'space'
     folder_path = os.path.join('images')
 
-    response = requests.get(api_space)
+    response = requests.get(api_space, timeout=60)
     response.raise_for_status()
-    if os.path.exists('images'):
-        pass
-    else:
-        os.mkdir('images')
+
+    os.makedirs('images', exist_ok=True)
     images = response.json()['links']['flickr']['original']
+
     for url_image in images:
         name_count = len(os.listdir(folder_path))
         save_image(url_image, folder_path, filename, name_count)
-
-
-fetch_spacex_last_launch()
