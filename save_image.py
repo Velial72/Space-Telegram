c933ae891fe96a9ad2ddd9cd22e73ea672533fa6
Velial72 <126498671+Velial72@users.noreply.github.com>
2023-03-27 20:13:39 +0300

    Update save_image.py
    упростил код

--- save_image.py
+++ save_image.py
@@ -4,12 +4,14 @@ import urllib
 
 
 def save_image(url, path, filename, name_count):
-    response = requests.get(url)
+    response = requests.get(url, timeout=60)
     response.raise_for_status()
     with open(os.path.join(path, f'{filename}_{name_count}.jpg'), 'wb') as file:
         file.write(response.content)
 
 
-def checking_the_extension(url):
-    extension = os.path.splitext((urllib.parse.urlsplit(url)[2]))[1]
+def check_the_extension(url):
+
+    api_file = urllib.parse.urlsplit(url)[2]
+    extension = os.path.splitext(api_file)[1]
     return extension
