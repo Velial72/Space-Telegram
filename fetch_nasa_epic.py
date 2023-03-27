3708ec65094a93751f7bea9243228335f54e7fdf
Velial72 <126498671+Velial72@users.noreply.github.com>
2023-03-27 20:11:53 +0300

    Update fetch_nasa_epic.py
    убрал лишнее из кода

--- fetch_nasa_epic.py
+++ fetch_nasa_epic.py
@@ -1,12 +1,25 @@
-def fetch_nasa_epic():
-    token = os.getenv('NASA_TOKEN')
+import os
+from save_image import save_image
+import requests
+import datetime
+
+
+def fetch_nasa_epic(token):
     filename = 'epic'
     payload = {"api_key": token}
     folder_path = os.path.join('images')
-    response = requests.get('https://api.nasa.gov/EPIC/api/natural/image', params=payload)
-    os.makedir('images')
+    response = requests.get('https://api.nasa.gov/EPIC/api/natural/image', params=payload, timeout=60)
+    os.makedirs('images', exist_ok=True)
+
+    today = datetime.date.today()
+    date_of_image = today - datetime.timedelta(days=1)
+    formatted_date = date_of_image.strftime("%Y/%m/%d")
     for name_image in response.json():
         name_count = len(os.listdir(folder_path))
-        info_image = name_image['image'], name_image['date'].split(' ')[0].replace('-', '/')
-        endpoint = f'https://api.nasa.gov/EPIC/archive/natural/{info_image[1]}/png/{info_image[0]}.png?api_key=DEMO_KEY'
+        info_image = name_image['image']
+
+        api_epic = requests.get(f'https://api.nasa.gov/EPIC/archive/natural/{formatted_date}/png/{info_image}.png',
+                                params=payload)
+        endpoint = api_epic.url
+
         save_image(endpoint, folder_path, filename, name_count)
