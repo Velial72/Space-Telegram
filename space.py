69036b8ed50b905c790313d0a9e6061e6ff4334a
Velial72 <126498671+Velial72@users.noreply.github.com>
2023-03-31 18:27:36 +0300

    Update space.py
    изменил на launch_id

--- space.py
+++ space.py
@@ -4,7 +4,7 @@ import argparse
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Введите ID последнего запуска')
-    parser.add_argument('id_launch', nargs='?', help='ID запуска', default='latest')
+    parser.add_argument('launch_id', nargs='?', help='ID запуска', default='latest')
     entered_values = parser.parse_args()
 
-    fetch_spacex_last_launch(entered_values.id_launch)
+    fetch_spacex_last_launch(entered_values.launch_id)
