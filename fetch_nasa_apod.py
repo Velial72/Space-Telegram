4ab92cb919f2645f737426feae76a30923971eee
Velial72 <126498671+Velial72@users.noreply.github.com>
2023-03-31 19:35:21 +0300

    Update fetch_nasa_apod.py
    исправил баг

--- fetch_nasa_apod.py
+++ fetch_nasa_apod.py
@@ -6,12 +6,12 @@ from dotenv import load_dotenv
 def fetch_nasa_apod(token):
     filename = 'nasa'
     folder_path = os.path.join('images')
+    os.makedirs('images', exist_ok=True)
     payload = {"api_key": token}
     name_count = len(os.listdir(folder_path))
 
     response = requests.get('https://api.nasa.gov/planetary/apod', params=payload, timeout=60)
     response.raise_for_status()
-    os.makedirs('images', exist_ok=True)
     file_extension = get_an_extension(response.json()['url'])
 
     if file_extension == '.jpg':
