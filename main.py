485c31bd50bef7dc2d22ddd7e659be23122ddd3c
Velial72 <126498671+Velial72@users.noreply.github.com>
2023-03-31 21:23:36 +0300

    Update main.py
    добавил Try... except

--- main.py
+++ main.py
@@ -13,16 +13,19 @@ if __name__ == '__main__':
     parser.add_argument('rate', nargs='?', help='частота публикаций', default='14400')
     entered_values = parser.parse_args()
     while True:
-        token = os.environ['TG_TOKEN']
-        folder_path = os.path.join('images')
-        bot = telegram.Bot(token=token)
-        tree = os.walk('images')
-
-        for root, catalog, images in tree:
-            for image in images:
-                path_to_image = os.path.join(root, image)
-                with open(path_to_image, 'rb') as file:
-                    bot.send_document(chat_id=entered_values.telegram_id, document=(file), timeout=60)
-                    telegram.error.BadRequest('Telegram не может корректно обработать запрос')
-                time.sleep(int(entered_values.rate))
-            random.shuffle(images)
+        try:
+            token = os.environ['TG_TOKEN']
+            folder_path = os.path.join('images')
+            bot = telegram.Bot(token=token)
+            tree = os.walk('images')
+            
+            for root, catalog, images in tree:
+                for image in images:
+                    path_to_image = os.path.join(root, image)
+                    with open(path_to_image, 'rb') as file:
+                        bot.send_document(chat_id=entered_values.telegram_id, document=(file), timeout=60)
+                        time.sleep(int(entered_values.rate))
+                random.shuffle(images)
+        except telegram.error.NetworkError() as error:
+            logging.error(error)
+            time.sleep(5)
