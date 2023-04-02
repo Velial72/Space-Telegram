c3cd1274269639b160012110db5338e8b529aacf
Velial72 <126498671+Velial72@users.noreply.github.com>
2023-04-02 07:56:58 +0300

    исправил наименования переменных
    конкретизировал наименования переменных сроки 7 и 18

--- fetch_spacex_last_launch.py
+++ fetch_spacex_last_launch.py
@@ -4,7 +4,7 @@ from save_image import save_image
 import argparse
 
 
-def fetch_spacex_last_launch(entered_values):
+def fetch_spacex_last_launch(launch_id):
     api_space = f'https://api.spacexdata.com/v5/launches/{entered_values}'
     filename = 'space'
     folder_path = os.path.join('images')
@@ -13,9 +13,9 @@ def fetch_spacex_last_launch(entered_values):
     response.raise_for_status()
 
     os.makedirs('images', exist_ok=True)
-    images = response.json()['links']['flickr']['original']
+    images_url = response.json()['links']['flickr']['original']
 
-    for image_url in images:
+    for image_url in images_url:
         name_count = len(os.listdir(folder_path))
         save_image(image_url, folder_path, filename, name_count)
        
