d51131645445df4a1dd9f8b355d716643e9de3a9
Velial72 <126498671+Velial72@users.noreply.github.com>
2023-04-02 09:01:42 +0300

    изменил получение ID канала
    изменил получение ID канала, теперь берется из .env

--- main.py
+++ main.py
@@ -9,12 +9,12 @@ import time
 if __name__ == '__main__':
     load_dotenv()
     parser = argparse.ArgumentParser(description='Введите: ID канала телеграм и частоту публикаций в секундах')
-    parser.add_argument('telegram_id', nargs='?', help='ID канала', default='@space464')
     parser.add_argument('rate', nargs='?', type=int, help='частота публикаций', default='14400')
     entered_values = parser.parse_args()
     while True:
         try:
             token = os.environ['TG_TOKEN']
+            chat_id = os.environ['CHAT_ID']
             folder_path = os.path.join('images')
             bot = telegram.Bot(token=token)
             tree = os.walk('images')
@@ -23,7 +23,7 @@ if __name__ == '__main__':
                 for image in images:
                     path_to_image = os.path.join(root, image)
                     with open(path_to_image, 'rb') as file:
-                        bot.send_document(chat_id=entered_values.telegram_id, document=file, timeout=60)
+                        bot.send_document(chat_id=chat_id, document=file, timeout=60)
                         time.sleep(entered_values.rate)
                 random.shuffle(images)
                 telegram.error.NetworkError()
